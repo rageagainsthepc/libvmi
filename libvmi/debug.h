ac7109bc680116ebd8ba8bfa44362c6794f4847a
Mathieu Tarral <mathieu.tarral@protonmail.com>
2017-05-17 08:20:36 -0700

    enable debug output for core messages and KVM driver

--- libvmi/debug.h
+++ libvmi/debug.h
@@ -54,6 +54,6 @@ typedef enum {
 } vmi_debug_flag_t;
 
 /* uncomment this and recompile to enable debug output */
-//#define VMI_DEBUG __VMI_DEBUG_ALL
+#define VMI_DEBUG (VMI_DEBUG_CORE | VMI_DEBUG_KVM)
 
 #endif
